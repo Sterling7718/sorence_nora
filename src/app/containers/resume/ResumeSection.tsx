445289e55a8d0480a11fc47d6816037a2bafd301
Sterling7718 <nora.sorence@gmail.com>
2024-09-02 13:20:40 +0800

    Added "mb-6" class to main element in ResumeSection component.

--- src/app/containers/resume/ResumeSection.tsx
+++ src/app/containers/resume/ResumeSection.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const ResumeSection = () => {
   return (
-    <main className="w-full h-screen flex items-start justify-center">
+    <main className="w-full h-screen flex items-start justify-center mb-6">
       <Header title={"About me"} />
     </main>
   );
