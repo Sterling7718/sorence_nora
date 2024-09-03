2a691ce6f0cc71faf3d927d41e425f327d2d649c
Sterling7718 <nora.sorence@gmail.com>
2024-09-03 09:32:48 +0800

    "Updated CircuitBorder component to accept a 'flip' prop, added Google Fonts stylesheet, and refactored ResumeSection component with new layout and styling."

--- src/app/containers/resume/CircuitBorder.tsx
+++ src/app/containers/resume/CircuitBorder.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 
-const CircuitBorder: React.FC = () => {
+interface CircuitBorderProps {
+  flip: boolean; // Adding a boolean prop named 'flip'
+}
+
+const CircuitBorder: React.FC<CircuitBorderProps> = ({ flip }) => {
   return (
-    <div className="z-10 border relative h-[24px] w-[650px] flex items-center justify-center">
-      {/* Horizontal line with circles */}
-      <div className="absolute -top-[170px] left-[10px] w-9 h-9 bg-foregroundlightcyan rounded-full flex items-center justify-center">
-        <div className="w-5 h-5 bg-bg rounded-full"></div>{" "}
-      </div>
-      <div className="absolute -top-[140px] left-[20px] w-4 h-[159px] bg-foregroundlightcyan"></div>
+    <div
+      className={` ${flip ? "relative scale-x-[-1] -left-0.5" : ""}`} // Conditionally adding 'scale-x-[-1]' class based on the 'flip' prop
+    >
+      <div
+        id="main"
+        className="z-10 relative h-[24px] w-[650px] flex items-center justify-center"
+      >
+        {/* Horizontal line with circles */}
+        <div className="absolute -top-[170px] left-[54px] w-9 h-9 bg-foregroundlightcyan rounded-full flex items-center justify-center">
+          <div className="w-5 h-5 bg-bg rounded-full"></div>
+          <div className="absolute top-[29px] left-[10px] w-4 h-[160px] bg-foregroundlightcyan"></div>
+        </div>
 
-      <div className="w-[600px] h-4 bg-foregroundlightcyan flex items-center justify-center"></div>
+        <div className="w-[520px] h-4 bg-foregroundlightcyan flex items-center justify-center"></div>
 
-      <div className="absolute top-[170px] right-[10px] w-9 h-9 bg-foregroundlightcyan rounded-full flex items-center justify-center">
-        <div className="w-5 h-5 bg-bg rounded-full"></div>{" "}
+        <div className="absolute top-[165px] right-[55px] w-9 h-9 bg-foregroundlightcyan rounded-full flex items-center justify-center">
+          <div className="w-5 h-5 bg-bg rounded-full"></div>
+          <div className="absolute -top-[152px] left-[10px] w-4 h-[159px] bg-foregroundlightcyan"></div>
+        </div>
       </div>
-      <div className="absolute top-[3px] right-[20px] w-4 h-[159px] bg-foregroundlightcyan"></div>
     </div>
   );
 };
--- src/app/containers/resume/ResumeSection.tsx
+++ src/app/containers/resume/ResumeSection.tsx
@@ -5,6 +5,11 @@ import CircuitBorder from "./CircuitBorder";
 const ResumeSection = () => {
   return (
     <main className="w-full h-auto flex flex-col items-center justify-center mb-6">
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,600,1,0"
+      />
+
       {/* Centered the entire main content */}
       <Header title={"About me"} />
       <div className="w-full h-auto flex flex-row items-center justify-center mb-24">
@@ -16,10 +21,8 @@ const ResumeSection = () => {
           id="descriptions"
           className="bg-forbentobg p-7 rounded-3xl  w-fit flex flex-col items-center"
         >
-          {/* Added flex and items-center to center the paragraph within the div */}
-          <div className="flex justify-center text-center">
-            {/* Adjusted width to full and text alignment to left for the paragraph */}
-            <div className="text-poppins1rem text-left grid grid-cols-2 gap-x-[1px]">
+          <div className="w-[28rem] flex justify-center text-center">
+            <div className="w-[26rem] text-poppins1rem text-left grid grid-cols-2 gap-x-[1px]">
               <p>FirstName</p>
               <p>= Sorence</p>
               <p>LastName</p>
@@ -44,43 +47,114 @@ const ResumeSection = () => {
           </a>
         </div>
       </div>
-      <CircuitBorder />
-      <div className="w-full h-auto flex flex-row items-center justify-center my-24">
-        {/* Adjusted to flex-col for vertical alignment */}
+      <CircuitBorder flip={false} />
+      <div
+        id="TitleAndDesc"
+        className="w-full h-auto flex flex-row items-center justify-center my-24"
+      >
         <div
-          id="descriptions"
-          className="bg-forbentobg p-7 rounded-3xl  w-fit flex flex-col items-center"
+          id="TitleAndDesc"
+          className="bg-forbentobg p-7 rounded-3xl w-[30rem] h-[20rem] flex flex-col items-center text-center justify-center"
         >
-          {/* Added flex and items-center to center the paragraph within the div */}
-          <div className="flex justify-center text-center">
-            {/* Adjusted width to full and text alignment to left for the paragraph */}
-            <div className="text-poppins1rem text-left grid grid-cols-2 gap-x-[1px]">
-              <p>FirstName</p>
-              <p>= Sorence</p>
-              <p>LastName</p>
-              <p>= Nora</p>
-              <p>CountryOrigin</p>
-              <p>= Philippines</p>
-              <p>Email</p>
-              <p>= nora.sorence@gmail.com</p>
-              <p>Phone</p>
-              <p>= +63 993 841 3918</p>
-              <p>Languages</p>
-              <p>= Filipino, English</p>
+          <div id="2022-2024" className="text-start mb-5 w-[410px]">
+            <div id="year" className="flex justify-start">
+              <div className="bg-green-900 flex justify-center items-center w-10 h-10 rounded-full">
+                <span className="material-symbols-outlined">school</span>
+              </div>
+              <h3 className="text-poppins1_5rem font-semibold mb-1 ml-1">
+                2022-2024
+              </h3>
+              <div className="relative">
+                <div className="absolute top-[2.6rem] right-[8.7rem] bg-white w-0.5 h-20"></div>
+              </div>
+            </div>
+            <div id="desc" className="ml-20">
+              <h4 className="text-poppins1rem font-medium ">
+                ASSOCIATES IN COMPUTER TECHNOLOGY <br />- STI SAN JOSE DEL MONTE
+              </h4>
+              <p className="text-poppins1rem ml-5  ">2 years college program</p>
             </div>
           </div>
-          <a
-            href="https://drive.google.com/file/d/1_2AFU6mu0gYI23akwfE4JxWfK6lw1ITj/view?usp=sharing"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-700 transition duration-300"
-          >
-            View My Resume
-          </a>
-        </div>{" "}
+
+          <div id="2022-2024" className="text-start mb-4 w-[410px]">
+            <div id="year" className="flex justify-start">
+              <div className="bg-green-900 flex justify-center items-center w-10 h-10 rounded-full">
+                <span className="material-symbols-outlined">school</span>
+              </div>
+              <h3 className="text-poppins1_5rem font-semibold mb-1 ml-1">
+                2020-2022
+              </h3>
+              <div className="relative">
+                <div className="absolute top-[2.6rem] right-[8.7rem] bg-white w-0.5 h-20"></div>
+              </div>
+            </div>
+            <div id="desc" className="ml-20">
+              <h4 className="text-poppins1rem font-medium ">
+                IT IN MOBILE APP AND WEB DEVELOPMENT
+                <br />- STI FAIRVIEW
+              </h4>
+              <p className="text-poppins1rem ml-5  ">
+                Senior high school program (Grade 11 - 12)
+              </p>
+            </div>
+          </div>
+        </div>
         <h1 className="text-subheadingpoppins2rem font-semibold ml-16 text-center">
-          &lt;Personal Info /&gt;
+          &lt;Education /&gt;
         </h1>
+      </div>
+      <CircuitBorder flip={true} />
+      <div className="w-full h-auto flex flex-row items-center justify-center my-24">
+        <h1 className="text-subheadingpoppins2rem font-semibold mr-16 text-center">
+          &lt;Education /&gt;
+        </h1>
+        <div
+          id="Desc"
+          className="bg-forbentobg p-7 rounded-3xl w-[30rem] h-[20rem] flex flex-col items-center text-center justify-center"
+        >
+          <div id="2022-2024" className="text-start mb-5 w-[410px]">
+            <div id="year" className="flex justify-start">
+              <div className="bg-green-900 flex justify-center items-center w-10 h-10 rounded-full">
+                <span className="material-symbols-outlined">school</span>
+              </div>
+              <h3 className="text-poppins1_5rem font-semibold mb-1 ml-1">
+                2022-2024
+              </h3>
+              <div className="relative">
+                <div className="absolute top-[2.6rem] right-[8.7rem] bg-white w-0.5 h-20"></div>
+              </div>
+            </div>
+            <div id="desc" className="ml-20">
+              <h4 className="text-poppins1rem font-medium ">
+                ASSOCIATES IN COMPUTER TECHNOLOGY <br />- STI SAN JOSE DEL MONTE
+              </h4>
+              <p className="text-poppins1rem ml-5  ">2 years college program</p>
+            </div>
+          </div>
+
+          <div id="2022-2024" className="text-start mb-4 w-[410px]">
+            <div id="year" className="flex justify-start">
+              <div className="bg-green-900 flex justify-center items-center w-10 h-10 rounded-full">
+                <span className="material-symbols-outlined">school</span>
+              </div>
+              <h3 className="text-poppins1_5rem font-semibold mb-1 ml-1">
+                2020-2022
+              </h3>
+              <div className="relative">
+                <div className="absolute top-[2.6rem] right-[8.7rem] bg-white w-0.5 h-20"></div>
+              </div>
+            </div>
+            <div id="desc" className="ml-20">
+              <h4 className="text-poppins1rem font-medium ">
+                IT IN MOBILE APP AND WEB DEVELOPMENT
+                <br />- STI FAIRVIEW
+              </h4>
+              <p className="text-poppins1rem ml-5  ">
+                Senior high school program (Grade 11 - 12)
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
