13914328303fb6e515ecac87392df6ee068eeb6b
Sterling7718 <nora.sorence@gmail.com>
2024-09-05 12:39:52 +0800

    "Added Formspree integration to ContactSection component"

--- src/app/containers/contact/ContactSection.tsx
+++ src/app/containers/contact/ContactSection.tsx
@@ -1,11 +1,19 @@
 import Header from "@/app/components/Header";
 import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 
 const ContactSection = () => {
+  const [state, handleSubmit] = useForm("xeojdzak");
+  if (state.succeeded) {
+    return <p>Thanks for messaging!</p>;
+  }
   return (
     <main className="mb-6 flex h-auto w-full flex-col items-center justify-start">
       <Header title={"Message me"} />
-      <form className="flex h-auto w-[30rem] flex-col items-center justify-center space-y-4 rounded-3xl p-7 text-center">
+      <form
+        onSubmit={handleSubmit}
+        className="flex h-auto w-[30rem] flex-col items-center justify-center space-y-4 rounded-3xl p-7 text-center"
+      >
         {/* Name Input */}
         <input
           type="text"
@@ -17,27 +25,37 @@ const ContactSection = () => {
         <input
           type="text"
           placeholder="Email"
+          name="email"
           className="w-full rounded-lg bg-forbentobg p-3 text-poppins1rem placeholder:text-gray-500"
         />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
         {/* Subject Input */}
         <input
           type="text"
           placeholder="Subject"
+          name="subject"
           className="w-full rounded-lg bg-forbentobg p-3 text-poppins1rem placeholder:text-gray-500"
         />
         {/* Message Textarea */}
         <textarea
+          id="message"
+          name="message"
           placeholder="Message"
           className="h-32 w-full resize-none rounded-lg bg-forbentobg p-3 text-poppins1rem placeholder:text-gray-500"
-        ></textarea>
+        />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
         {/* Send Button */}
         <button
           type="submit"
           className="mt-4 rounded-full bg-blue-500 px-7 py-2 text-foregroundparchment transition duration-300 hover:bg-blue-700"
+          disabled={state.submitting}
         >
           Send
         </button>
-        {/* Button with hover effect */}
       </form>
     </main>
   );
