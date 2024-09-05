e29c4063bbfaf144f06396e13d4089ed5e1d6665
Sterling7718 <nora.sorence@gmail.com>
2024-09-05 20:02:48 +0800

    "Refactor SkillCards component to use CSS variables for dynamic border color and drop shadow, and update Tailwind CSS classes for layout, size, border, and hover effects."

--- src/app/containers/skills/components/SkillCards.tsx
+++ src/app/containers/skills/components/SkillCards.tsx
@@ -11,33 +11,29 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ src, borderColor, tooltipContent }) => {
   return (
     <div
-      className={`m-1 flex h-[110px] w-[95px] flex-col items-center justify-center rounded-lg border-4 transition-all duration-300`}
-      style={{
-        borderColor: borderColor,
-      }}
+      style={
+        {
+          "--border-color": borderColor, // Using the dynamic border color
+          "--drop-shadow": `drop-shadow(0 0 10px ${borderColor})`, // Dynamic drop-shadow color based on the borderColor
+          borderColor: `var(--border-color)`, // Applying the custom CSS variable to the border color
+        } as React.CSSProperties // Casting to React.CSSProperties to allow custom CSS variables
+      }
+      className={`m-1 flex h-[110px] w-[95px] flex-col items-center justify-center rounded-lg border-4 transition-all duration-300 hover:-skew-x-6 hover:skew-y-3 hover:[filter:var(--drop-shadow)]`}
     >
       <div
-        className={`transition-all duration-300 hover:shadow-lg`}
-        // Tailwind CSS classes for layout, size, border, and hover effect
-        style={
-          {
-            "--border-color": borderColor, // Using the dynamic border color
-            "--drop-shadow": `drop-shadow(0 0 10px ${borderColor})`, // Dynamic drop-shadow color based on the borderColor
-            borderColor: `var(--border-color)`, // Applying the custom CSS variable to the border color
-          } as React.CSSProperties // Casting to React.CSSProperties to allow custom CSS variables
-        }
+      // Tailwind CSS classes for layout, size, border, and hover effect
       >
         <div className="transition-all duration-300 hover:[filter:var(--drop-shadow)]">
           <Tooltip
             content={tooltipContent}
-            className="max-w-fit bg-bg px-3 py-2"
+            className="max-w-fit rounded-md bg-bg px-3 py-2 opacity-90"
           >
             <Image
               src={src}
               width={50}
               height={50}
               alt="Image"
-              className="rounded-lg"
+              className="rounded-lg transition-all duration-300 hover:-skew-x-6 hover:skew-y-3 hover:animate-pulse"
             />
           </Tooltip>
         </div>
