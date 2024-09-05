a566430e4504f6f578432f0937173253a1ae9251
Sterling7718 <nora.sorence@gmail.com>
2024-09-05 20:07:30 +0800

    Added tooltip content to TechCards component and updated styles for cards in ProjectsSection.

--- src/app/containers/projects/ProjectsSection.tsx
+++ src/app/containers/projects/ProjectsSection.tsx
@@ -40,31 +40,42 @@ const ProjectsSection = () => {
                     <TechCards
                       src="/techLogos/Html5.svg"
                       borderColor="#E34F26"
+                      tooltipContent={"HTML"}
                     />
-                    <TechCards src="/techLogos/css.svg" borderColor="#264DE4" />
+                    <TechCards
+                      src="/techLogos/css.svg"
+                      borderColor="#264DE4"
+                      tooltipContent={"CSS"}
+                    />
                     <TechCards
                       src="/techLogos/Tailwind.svg"
                       borderColor="#38BDF8"
+                      tooltipContent={"Tailwind"}
                     />
                     <TechCards
                       src="/techLogos/JavaScript.svg"
                       borderColor="#F7DF1E"
+                      tooltipContent={"JavaScript"}
                     />
                     <TechCards
                       src="/techLogos/Typescript.svg"
                       borderColor="#3178C6"
+                      tooltipContent={"Typescript"}
                     />
                     <TechCards
                       src="/techLogos/Nextjs.svg"
                       borderColor="#FFFFFF"
+                      tooltipContent={"Nextjs"}
                     />
                     <TechCards
                       src="/techLogos/Supabase.svg"
                       borderColor="#3ECF8E"
+                      tooltipContent={"Supabase"}
                     />
                     <TechCards
                       src="/techLogos/Postgresql.svg"
                       borderColor="#336791"
+                      tooltipContent={"PostgreSQL"}
                     />
                   </div>
                 </div>
--- src/app/containers/projects/components/TechCards.tsx
+++ src/app/containers/projects/components/TechCards.tsx
@@ -1,24 +1,43 @@
 import React from "react";
 import Image from "next/image";
+import { Tooltip } from "@nextui-org/tooltip";
 
 interface CardProps {
-  src: string;
-  borderColor: string;
+  src: string; // Source for the image
+  borderColor: string; // Border color
+  tooltipContent: string;
 }
 
-const Card: React.FC<CardProps> = ({ src, borderColor }) => {
+const Card: React.FC<CardProps> = ({ src, borderColor, tooltipContent }) => {
   return (
     <div
-      className="w-[50px] h-[65px] m-1 rounded-lg border-2 flex justify-center items-center"
-      style={{ borderColor }}
+      style={
+        {
+          "--border-color": borderColor, // Using the dynamic border color
+          "--drop-shadow": `drop-shadow(0 0 10px ${borderColor})`, // Dynamic drop-shadow color based on the borderColor
+          borderColor: `var(--border-color)`, // Applying the custom CSS variable to the border color
+        } as React.CSSProperties // Casting to React.CSSProperties to allow custom CSS variables
+      }
+      className={`m-1 flex h-[65px] w-[50px] flex-col items-center justify-center rounded-lg border-4 transition-all duration-300 hover:-skew-x-6 hover:skew-y-3 hover:[filter:var(--drop-shadow)]`}
     >
-      <Image
-        src={src}
-        width={25}
-        height={25}
-        alt="Tech Logo"
-        className="rounded-lg"
-      />
+      <div
+      // Tailwind CSS classes for layout, size, border, and hover effect
+      >
+        <div className="transition-all duration-300 hover:[filter:var(--drop-shadow)]">
+          <Tooltip
+            content={tooltipContent}
+            className="max-w-fit rounded-md bg-bg px-3 py-2 opacity-90"
+          >
+            <Image
+              src={src}
+              width={25}
+              height={25}
+              alt="Image"
+              className="rounded-lg transition-all duration-300 hover:-skew-x-6 hover:skew-y-3 hover:animate-pulse"
+            />
+          </Tooltip>
+        </div>
+      </div>
     </div>
   );
 };
