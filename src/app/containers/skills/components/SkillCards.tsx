15adff471f2564a6886d7c9b779d26f8ab39e53d
Sterling7718 <nora.sorence@gmail.com>
2024-09-05 18:42:22 +0800

    "Added transition and drop-shadow styles to SkillCards component"

--- src/app/containers/skills/components/SkillCards.tsx
+++ src/app/containers/skills/components/SkillCards.tsx
@@ -9,16 +9,25 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ src, borderColor }) => {
   return (
     <div
-      className={`m-1 flex h-[110px] w-[95px] flex-col items-center justify-center rounded-lg border-4`}
-      style={{ borderColor: borderColor }}
+      className={`m-1 flex h-[110px] w-[95px] flex-col items-center justify-center rounded-lg border-4 transition-all duration-300`}
+      style={{
+        borderColor: borderColor,
+      }}
     >
-      <Image
-        src={src}
-        width={50}
-        height={50}
-        alt="Image"
-        className="rounded-lg"
-      />
+      <div
+        className="transition-all duration-300"
+        style={{
+          filter: `drop-shadow(0 0 10px ${borderColor})`, // Apply the glow effect using drop-shadow
+        }}
+      >
+        <Image
+          src={src}
+          width={50}
+          height={50}
+          alt="Image"
+          className="rounded-lg"
+        />
+      </div>
     </div>
   );
 };
