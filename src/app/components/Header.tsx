0ce9cde02cd3b34c6068a04ba94ae3464f6c8a7c
Sterling7718 <nora.sorence@gmail.com>
2024-09-07 09:32:37 +0800

    "Updated font size and positioning of header title and underline"

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,9 +7,9 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
     <header className="mb-6 mt-[71px] flex justify-start">
-      <h2 className="relative z-10 inline-block text-3xl font-bold md:text-subheadingpoppins3rem">
+      <h2 className="relative z-10 inline-block text-2xl font-bold md:text-[4vw]">
         {title}
-        <span className="absolute left-1/2 top-8 -z-10 h-[4px] w-full -translate-x-1/2 transform animate-pulse bg-[#00FFFF] opacity-100 shadow-[0_-5px_10px_#00FFFF] blur-[1px] md:top-9"></span>
+        <span className="absolute left-1/2 top-[7vw] -z-10 h-[4px] w-full -translate-x-1/2 transform animate-pulse bg-[#00FFFF] opacity-100 shadow-[0_-5px_10px_#00FFFF] blur-[1px] md:top-[4vw] lg:top-[3vw]"></span>
       </h2>
     </header>
   );
