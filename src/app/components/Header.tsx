3e284864f364794940f6e449ba7d194301dde08a
Sterling7718 <nora.sorence@gmail.com>
2024-09-07 09:20:04 +0800

    "Update Header component: changed margin and font size, adjusted position and styling of animated underline"

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,10 +6,10 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
-    <header className="mb-2 mt-[71px] flex justify-start">
-      <h2 className="relative z-10 inline-block text-subheadingpoppins3rem font-bold">
+    <header className="mb-6 mt-[71px] flex justify-start">
+      <h2 className="relative z-10 inline-block text-3xl font-bold md:text-subheadingpoppins3rem">
         {title}
-        <span className="absolute bottom-2 left-1/2 -z-10 h-[4px] w-full -translate-x-1/2 transform animate-pulse bg-[#00FFFF] opacity-100 shadow-[0_-5px_10px_#00FFFF] blur-[1px]"></span>
+        <span className="absolute left-1/2 top-8 -z-10 h-[4px] w-full -translate-x-1/2 transform animate-pulse bg-[#00FFFF] opacity-100 shadow-[0_-5px_10px_#00FFFF] blur-[1px] md:top-9"></span>
       </h2>
     </header>
   );
